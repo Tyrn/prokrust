5f46b7e3191c9a5ad759f926df00e96682358f9a
Tyrn <a@b.com>
2024-01-16 17:34:41 +0300

    clikt used for the first time

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,7 +1,11 @@
+import com.github.ajalt.clikt.core.CliktCommand
+
 fun initials(names: String): String {
    return names
 }
-
-fun main() {
-    println("Hello, Baeldung!")
+class Shoot: CliktCommand() {
+    override fun run() {
+        echo("¡Hola, Kitty!")
+    }
 }
+fun main(args: Array<String>) = Shoot().main(args)
