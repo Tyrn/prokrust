31e4fd408d02567b6a11d5102a3d1646945a7fb1
Tyrn <a@b.com>
2024-01-17 19:08:41 +0300

    formInitial() before testing

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,12 +1,77 @@
 import com.github.ajalt.clikt.core.CliktCommand
 
+val nobiliaryParticles = arrayOf(
+    "von",
+    "фон",
+    "van",
+    "ван",
+    "der",
+    "дер",
+    "til",
+    "тиль",
+    "zu",
+    "цу",
+    "zum",
+    "цум",
+    "zur",
+    "цур",
+    "af",
+    "аф",
+    "of",
+    "из",
+    "da",
+    "да",
+    "de",
+    "де",
+    "des",
+    "дез",
+    "del",
+    "дель",
+    "di",
+    "ди",
+    "dos",
+    "душ",
+    "дос",
+    "du",
+    "дю",
+    "la",
+    "ла",
+    "ля",
+    "le",
+    "ле",
+    "haut",
+    "от",
+    "the",
+)
 val rDots = "[\\s.]+".toRegex()
 val rQuotedSubstrings = """"(?:\\.|[^"\\])*"""".toRegex()
 
 fun initials(authors: String): String {
     fun formInitial(name: String): String {
-        if (name.isNotEmpty()) return name[0].toString().uppercase()
-        return "*"
+        val cut = name.split("'")
+        if (cut.size > 1 && cut[1].isNotEmpty()) {  // Deal with '.
+            if (cut[1][0].isLowerCase() && cut[0].isNotEmpty()) {
+                return cut[0][0].uppercase()
+            }
+            return cut[0] + "'" + cut[1][0]
+        }
+        if (name.length > 1) {
+            when (name) {
+                "Старший" -> return "Ст"
+                "Младший" -> return "Мл"
+                "Ст", "ст", "Sr", "Мл", "мл", "Jr" -> return name
+            }
+        }
+        var head = name[0].toString()
+        val tail = name.slice(1..name.lastIndex)
+        for (ch in tail) {
+            head += ch.toString()
+            if (ch.isUpperCase()) return head
+        }
+        if (name in nobiliaryParticles) {
+            return name[0].toString()
+        }
+        return name[0].toString().uppercase()
     }
     return authors
         .replace(rQuotedSubstrings, " ")
