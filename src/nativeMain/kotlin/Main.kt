7ad169cbb84ef3e8b2ac74de176ffb48fe29fa71
Tyrn <a@b.com>
2024-01-17 16:28:09 +0300

    formInitial() started

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -4,11 +4,17 @@ val rDots = """[\s.]+""".toRegex()
 val rQuotedSubstrings = """"(?:\\.|[^"\\])*"""".toRegex()
 
 fun initials(names: String): String {
-   return names
+    fun formInitial(name: String): String {
+        if (name.isNotEmpty()) return name[0].toString().uppercase()
+        return ""
+    }
+    return names
 }
-class Shoot: CliktCommand() {
+
+class Shoot : CliktCommand() {
     override fun run() {
         echo("¡Hola, Kitty!")
     }
 }
+
 fun main(args: Array<String>) = Shoot().main(args)
