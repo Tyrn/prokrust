9543e4a65ab7670a49d2dbb3f3a6decb768617f1
Tyrn <a@b.com>
2024-01-17 22:35:54 +0300

    JAudioTagger 3.0.1 added

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
         val nativeMain by getting {
             dependencies {
                 implementation("com.github.ajalt.clikt:clikt:4.2.2")
+                implementation("net.jthink:jaudiotagger:3.0.1")
             }
         }
         val nativeTest by getting {
